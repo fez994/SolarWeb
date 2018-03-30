1410504c5cfd25a64aa50509bb96fc3eb5b1df49
Federico Dente <dentefederico@live.com>
2018-03-30 15:10:44 +0200

    added the Solcast request

--- js/index.js
+++ js/index.js
@@ -1,48 +1,39 @@
-$(document).ready(function(){
 
-//Global Vars 
-var address;
-var lat;
-var lon;
-var get;
+$(document).ready(function(){
 
 
 $('#btn').click(function() {
+
+	lfc();
+
+	});	// end btn function
+	
+
+function lfc () {
 	// getting the address from the form
 	var objAddress = $('#address').serializeArray();
 	var address = objAddress[0].value;
 	// Turning the Address to long and lat coordinates using the google maps api
 	var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyAIACAlD9aOoq-pox0MGpo3bTM5aplo_Jg";
-	var get = $.getJSON(url, function(data) {
-	// getting lat and long from the address
+	$.getJSON(url, function(data) {
+
 	var lat = data.results[0].geometry.location.lat;
 	var lon = data.results[0].geometry.location.lng;
 
-	// Requesting data from the solcast API
-	var solcast = "https://api.solcast.com.au/radiation/forecasts?longitude="+lon+"&latitude="+lat+"&api_key=fB0Zk_1cWKWI8clCSe98ZyBTWctbcyR3";
-	
-	var myData = $.getJSON(solcast, function(files) {
-		console.log(files);
+	useCoordinates(lat, lon);
 	});
 
+	// requesting data from the lat and lon coordinades 
+	function useCoordinates(lat, lon){
+		var myApi = "https://api.solcast.com.au/radiation/forecasts?longitude="+lon+"&latitude="+lat+"&api_key=fB0Zk_1cWKWI8clCSe98ZyBTWctbcyR3&format=json&callback=?";
+		$.getJSON(myApi, function(forecasts) {
+			console.log(forecasts);
+		});
+
+	}
 
 
-
-
-	});	// end get function
-	
-
-
-	
-}); // end btn click function
-
-
-
-
-
-
-
-
+} // end lfc
 
 
 
