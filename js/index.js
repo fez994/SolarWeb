571afefbb9aad14fd9b966b997d23fe029f117bc
Federico Dente <dentefederico@live.com>
2018-04-06 12:14:00 +0200

    script now return ghi, dni and dhi plus their sum

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 
 
@@ -16,22 +15,62 @@ function lfc () {
 	// Turning the Address to long and lat coordinates using the google maps api
 	var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyAIACAlD9aOoq-pox0MGpo3bTM5aplo_Jg";
 	$.getJSON(url, function(data) {
-
 	var lat = data.results[0].geometry.location.lat;
 	var lon = data.results[0].geometry.location.lng;
 
 	useCoordinates(lat, lon);
 	});
-	
+
 	// requesting data from the lat and lon coordinades 
-	function useCoordinates(lat, lon){
+	function useCoordinates(lat, lon){ 
+
 		const proxyurl = "https://cors-anywhere.herokuapp.com/";
 		const url = 'https://api.solcast.com.au/radiation/forecasts?format=json&latitude=' + lat +'&longitude='+ lon +'&api_key=fB0Zk_1cWKWI8clCSe98ZyBTWctbcyR3'; // site that doesn’t send Access-Control-*
 		fetch(proxyurl + url)
-		.then(response => response.text())
-		.then(contents => alert(contents))
-		.catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+		.then(response => response.json())
+		.then(contents =>  {
+			console.log("The global Horizontal Irradiance is " + contents.forecasts[0].ghi + " W/m2");
+			console.log("The direct normal irradiance is " + contents.forecasts[0].dni + " W/m2");
+			console.log("The diffuse horizontal irradiance is " + contents.forecasts[0].dhi + " W/m2");
+			var iTot  = eval(contents.forecasts[0].ghi + contents.forecasts[0].dni + contents.forecasts[0].dhi);
+			console.log(iTot);
+
+		})
+		.catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"));
 	}
+	
 } // end lfc
 
+
+
 }); // end document ready function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
