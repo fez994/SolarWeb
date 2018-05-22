0a6dd28b3be6ab16efce94f198f908d0c63b2cfa
fez994 <dentefederico@live.com>
2018-05-22 14:26:00 +0200

    fixed typos, formatted html, added comments

--- js/index.js
+++ js/index.js
@@ -1,33 +1,31 @@
 $(document).ready(function(){
 
 // Hiding the data div to display it later
-//$('.actuals').hide();
 $('.plant').hide();
 
 
 
 // Dropdown Menu
-
 $(".dropdown-menu li a").click(function(){
   $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
   $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+  // Type of pv
   window.g = $(this).attr("value");
+  // Performance percentage
   window.n = $(this).attr("data-value");
-  window.v = $(this).attr("title");
-		//console.log(v);
+  // Performance percentage divided by 100, so if performance 16%  => 0.16
+  window.v = $(this).attr("title");	
 });
 
 
-
+// starting lfc when the search button is pressed
 $('#btn').click(function() {
-
 	lfc();
-
 	});	// end btn function
 	
 
 function lfc () {
-	// getting the address from the form
+	// Getting the address from the form
 	var objAddress = $('#address').serializeArray();
 	var address = objAddress[0].value;
 	// Turning the Address to long and lat coordinates using the google maps api
@@ -35,46 +33,48 @@ function lfc () {
 	$.getJSON(url, function(data) {
 	var lat = data.results[0].geometry.location.lat;
 	var lon = data.results[0].geometry.location.lng;
-
 	useCoordinates(lat, lon);
 	});
 
-	// requesting data from the lat and lon coordinades 
 	function useCoordinates(lat, lon){ 
-
+		// Requesting SolCast data from the lat and lon coordinades 
 		const proxyurl = "https://cors-anywhere.herokuapp.com/";
 		const url = 'https://api.solcast.com.au/radiation/forecasts?format=json&latitude=' + lat +'&longitude='+ lon +'&api_key=fB0Zk_1cWKWI8clCSe98ZyBTWctbcyR3'; // site that doesn’t send Access-Control-*
 		fetch(proxyurl + url)
 		.then(response => response.json())
 		.then(contents =>  {
-			//console.log("The global Horizontal Irradiance is " + contents.forecasts[0].ghi + " W/m2");
-			//console.log("The direct normal irradiance is " + contents.forecasts[0].dni + " W/m2");
-			//console.log("The diffuse horizontal irradiance is " + contents.forecasts[0].dhi + " W/m2");
+			// Global Horizontal Irradiance
 			var ghi = contents.forecasts[0].ghi;
+			// Direct Normal Irradiance 
 			var dni = contents.forecasts[0].dni;
+			// Diffuse Horizontal Irradiance
 			var dhi = contents.forecasts[0].dhi;
 			useData(ghi, dni, dhi);
-			
 		})
 		.catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"));
 	}
 
 	// Displaying Data
 	function useData(ghi, dni, dhi) {
+		// Total Value of the Irradiance
 		var sum = ghi + dni + dhi;
+		// Getting the data of how many m^2 of panel the user have
 		var panels = $('#sqrm').serializeArray();
 		var sqrm = document.getElementById("sqrm").value;
+		// Displaying type of pv
 		$('#pv_type').text(g);
+		// Displaying performance in % of the pv
 		$('#performance').text(" is " + n);
+		// Displaying the total irradiance value
 		$('#irradiance-data').text(sum + " W/m^2 ");
-		$('#kw-available').text(Math.round(sum * v ) + " W/m^2 " + "You have a total of " + sqrm + " square meters of panels, wich means you're pv plant is producing  " + Math.round( sum * v * sqrm) + " W/m^2");
+		// Total irradiance * performance of the panel to get how much a single panel is producing
+		var totalKw = Math.round(sum * v);
+		// Displaying the data for the single panel and multiplying it for the number of square meters of solar panels
+		$('#kw-available').text(totalKw + " W/m^2 " + "You have a total of " + sqrm + " square meters of panels, wich means you're pv plant is producing  " + (totalKw * sqrm) + " W");
+		// Showing the data to the user
 		$('.actuals').removeClass('hidden');
-
-	}
-	
+	}	
 } // end lfc
-
-
 
 }); // end document ready function
 
